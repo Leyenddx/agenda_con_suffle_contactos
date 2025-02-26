42289a1fbebe6141f135f7c979e5d37b6b8e6ca1
Leyenddx <aarok_ny224@hotmail.com>
2025-02-26 10:14:46 -0700

    AGREGA
    un componente para la vista de contacto prevista

---
+++ Agenda_shuffle/Agenda_shuffle/componentes/contacto_prevista.swift
@@ -0,0 +1,22 @@
+//
+//  contacto_prevista.swift
+//  Agenda_shuffle
+//
+//  Created by alumno on 2/26/25.
+//
+
+import SwiftUI
+
+struct ContactoPrevista: View {
+    var body: some View {
+        HStack{
+            Image(systemName: "globe")
+            Text("Hello Darkness")
+            Text("My old friend")
+        }
+    }
+}
+
+#Preview {
+    ContactoPrevista()
+}
