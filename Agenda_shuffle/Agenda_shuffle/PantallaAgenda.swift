70901538197ac2cfbb64497f8e4b9f244b52a2c8
Leyenddx <aarok_ny224@hotmail.com>
2025-02-26 10:46:11 -0700

    [MODIFICA] la pantalla agenda para usar el modelo nuevo

--- Agenda_shuffle/Agenda_shuffle/PantallaAgenda.swift
+++ Agenda_shuffle/Agenda_shuffle/PantallaAgenda.swift
@@ -7,11 +7,23 @@
 
 import SwiftUI
 
+var contactos = [
+    ContactoAgenda(nombre:"Juan", telefono: "12345"),
+    ContactoAgenda(nombre:"Ju", telefono: "12345"),
+    ContactoAgenda(nombre:"Jan", telefono: "12345"),
+    ContactoAgenda(nombre:"Jun", telefono: "12345"),
+    ContactoAgenda(nombre:"Ja", telefono: "12345"),
+    ContactoAgenda(nombre:"Jn", telefono: "12345"),
+    ContactoAgenda(nombre:"Judsana", telefono: "12345"),
+]
+
+
 struct PantallaAgenda: View {
     var body: some View {
         List{
             VStack {
-                ForEach(0...45, id: \.self){ _ in
+                ForEach(contactos){ contacto in
+                    Text("\(contacto.nombre)")
                     ContactoPrevista()
                 }
             }
@@ -19,6 +31,8 @@ struct PantallaAgenda: View {
         }
     }
 }
+
+
 
 #Preview {
     PantallaAgenda()
