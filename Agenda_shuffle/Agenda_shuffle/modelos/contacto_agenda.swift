f3219db675214e1612cbc6b999cb32f5bece84b1
Leyenddx <aarok_ny224@hotmail.com>
2025-02-26 10:45:17 -0700

    [AGREGA] un modelo de contacto para guardar informacion

---
+++ Agenda_shuffle/Agenda_shuffle/modelos/contacto_agenda.swift
@@ -0,0 +1,32 @@
+//
+//  contacto_agenda.swift
+//  Agenda_shuffle
+//
+//  Created by alumno on 2/26/25.
+//
+
+import SwiftUI
+
+struct ContactoAgenda: Identifiable {
+    var nombre: String
+    var telefono: String
+    
+    var id: String { nombre }
+}
+
+
+
+
+/*
+var nombre = "Elvio Lado"
+
+var _nombre: String
+
+var nombre: set{
+    let nombre_validado = filtrar_palabras(value)
+            _nombre = nombre_validado
+} get {
+    return _nombre.split(" ")[0]
+}*/
+
+
