4a9994b3a54dbcdc3ed87477f1b777ee579c3be9
Leyenddx <aarok_ny224@hotmail.com>
2025-02-26 09:46:14 -0700

    [INICIALIZA] el proyecto de xcode

---
+++ Agenda_shuffle/Agenda_shuffle/Agenda_shuffleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Agenda_shuffleApp.swift
+//  Agenda_shuffle
+//
+//  Created by alumno on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Agenda_shuffleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PantallaAgenda()
+        }
+    }
+}
