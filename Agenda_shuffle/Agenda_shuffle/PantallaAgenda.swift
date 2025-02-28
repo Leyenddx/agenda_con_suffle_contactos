3010e42851c5176083abf2a432c6fca6b0479c4d
Leyenddx <aarok_ny224@hotmail.com>
2025-02-28 10:40:45 -0700

    [AGREGA] cositas nuevas

--- Agenda_shuffle/Agenda_shuffle/PantallaAgenda.swift
+++ Agenda_shuffle/Agenda_shuffle/PantallaAgenda.swift
@@ -15,20 +15,31 @@ var contactos = [
     ContactoAgenda(nombre:"Ja", telefono: "12345"),
     ContactoAgenda(nombre:"Jn", telefono: "12345"),
     ContactoAgenda(nombre:"Judsana", telefono: "12345"),
+    ContactoAgenda(nombre:"Judsana2", telefono: "12345"),
+    ContactoAgenda(nombre:"Judsana3", telefono: "12345"),
+    ContactoAgenda(nombre:"Judsana4", telefono: "12345"),
+    ContactoAgenda(nombre:"Judsana5", telefono: "12345"),
 ]
 
 
 struct PantallaAgenda: View {
+    var largo_de_pantalla = UIScreen.main.bounds.width
+    var ancho_de_pantalla = UIScreen.main.bounds.height
+    
     var body: some View {
-        List{
-            VStack {
+        
+        ScrollView{
+            VStack(spacing: 10) {
                 ForEach(contactos){ contacto in
-                    Text("\(contacto.nombre)")
-                    ContactoPrevista()
+                    //Text("\(contacto.nombre)")
+                    ContactoPrevista(contacto_a_mostrar: contacto, al_pulsar:{print("Te envia saludos \(contacto.nombre) desde la pantalla de agenda")})
                 }
             }
             .background(Color.cyan)
+            .frame(width: largo_de_pantalla, alignment: Alignment/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
         }
+        .background(Color.mint)
+       
     }
 }
 
--- Agenda_shuffle/Agenda_shuffle/componentes/contacto_prevista.swift
+++ Agenda_shuffle/Agenda_shuffle/componentes/contacto_prevista.swift
@@ -8,15 +8,44 @@
 import SwiftUI
 
 struct ContactoPrevista: View {
+    let esquinas_redondeadas = CGSize(width: 25, height: 25)
+    
+    var contacto_a_mostrar: ContactoAgenda
+    var al_pulsar: () -> Void = {print("No se ha implementado")}
+    
     var body: some View {
-        HStack{
-            Image(systemName: "globe")
-            Text("Hello Darkness")
-            Text("My old friend")
+        HStack(alignment: VerticalAlignment.center){
+            
+            Spacer()
+            
+            VStack{
+                Image("ImagenContacto")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 70, alignment: .center)
+                    .clipShape(RoundedRectangle(cornerSize: esquinas_redondeadas))
+            }.background(Color.blue)
+            
+            Spacer()
+            
+            VStack(alignment: HorizontalAlignment.leading) {
+                Text(contacto_a_mostrar.nombre)
+                Text(contacto_a_mostrar.telefono)
+            }
+            .background(Color.gray)
+            .frame(width: 150)
+            
+            Spacer()
+            
+        }
+        .frame(width: 250)
+        .background(Color.red)
+        .onTapGesture {
+            al_pulsar()
         }
     }
 }
 
 #Preview {
-    ContactoPrevista()
+    ContactoPrevista(contacto_a_mostrar: ContactoAgenda(nombre: "Chuyel nombre mas largo", telefono: "1234567"))
 }
